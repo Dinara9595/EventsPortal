711810dd393976f57e42b4f5713028f2b0d32233
Dinara9595 <dinara.petuhova95@yandex.ru>
2022-08-10 22:48:21 +0600

    Add date params to API

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,8 +2,6 @@ module Api
   module V1
     class EventsController < ApplicationController
       def index
-        events = Event.where(published: true)
-
         render json: EventSerializer.new(events)
       end
 
@@ -11,6 +9,18 @@ module Api
         event = Event.find(params[:id])
 
         render json: EventSerializer.new(event)
+      end
+
+      private
+
+      def events
+        events_published = Event.where(published: true)
+        start_date = params[:start_date]
+        end_date = params[:end_date]
+
+        events_published = events_published.where(created_at: start_date..end_date) if start_date && end_date
+
+        events_published
       end
     end
   end
